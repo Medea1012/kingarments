d5524c77cccd797264e78643d2c8c464bffcc60d
Medea Wang <medea.wangxy@gmail.com>
2023-12-13 13:58:51 -0500

    product change

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Product() {
-  return <div>Product</div>;
+  return (
+    <body>
+      <link
+        href="src/assets/Product.css"
+        type="text/css"
+        rel="stylesheet"
+      ></link>
+      <div className="text-white">Product</div>
+    </body>
+  );
 }
