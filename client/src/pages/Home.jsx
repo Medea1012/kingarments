89e2877e2fcfc97276a330b26c4abe6ce6a6effc
Medea Wang <medea.wangxy@gmail.com>
2023-12-24 14:10:27 -0500

    add backpack

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,63 @@
 export default function Home() {
   return (
     <div>
+      <div class="elementor-container">
+        <div
+          class="backpack"
+          // style="cursor: pointer;"
+          className="cursor:pointer;"
+          data-column-clickable="http://hagor.co.il/product-category/bags-and-backpacks/"
+          data-column-clickable-blank="_self"
+          data-id="51cf11f"
+          data-element_type="column"
+          data-settings='{"background_background":"classic"}'
+        >
+          <div class="elementor-widget-wrap elementor-element-populated">
+            <div class="elementor-background-overlay"></div>
+            <div
+              class="elementor-element elementor-element-7c94fd7 elementor-widget elementor-widget-heading"
+              data-id="7c94fd7"
+              data-element_type="widget"
+              data-widget_type="heading.default"
+            >
+              <div class="elementor-widget-container">
+                <h2 class="elementor-heading-title elementor-size-default">
+                  backpack
+                </h2>
+              </div>
+            </div>
+            <div
+              class="elementor-element elementor-element-f2d71db elementor-widget elementor-widget-text-editor"
+              data-id="f2d71db"
+              data-element_type="widget"
+              data-widget_type="text-editor.default"
+            >
+              <div class="elementor-widget-container">
+                <p>Winter Collection</p>
+              </div>
+            </div>
+            <div
+              class="elementor-element elementor-element-8a9a6e0 elementor-align-right cat-hover-btn elementor-widget elementor-widget-button"
+              data-id="8a9a6e0"
+              data-element_type="widget"
+              data-widget_type="button.default"
+            >
+              <div class="elementor-widget-container">
+                <div class="elementor-button-wrapper">
+                  <a
+                    class="elementor-button elementor-button-link elementor-size-sm"
+                    href="http://hagor.co.il/product-category/bags-and-backpacks/"
+                  >
+                    <span class="elementor-button-content-wrapper">
+                      <span class="elementor-button-text">SHOP NOW</span>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="py-20 px-6 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold mb-4 text-white">Who Are We</h1>
         <p className="mb-4">General Manager:</p>
