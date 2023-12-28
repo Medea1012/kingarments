cd3e0af3075b40207ba58349ef2b1d67ea73d034
Medea Wang <medea.wangxy@gmail.com>
2023-12-27 19:18:20 -0500

    add call us

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,45 @@
-import React from "react";
-
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section class="common-main">
+      <div class="left-wrap">
+        <div class="contact-list">
+          <div class="contact-item">
+            <div class="icon">
+              <img
+                alt=""
+                src="https://cdn.iconscout.com/icon/free/png-512/free-phone-1559-461603.png?f=webp&w=512"
+              />
+            </div>
+            <div class="text-list">
+              <p class="title">Call Us</p>
+              <div class="text-list-1">
+                <p class="text">
+                  China:
+                  <a href="tel:+86 13396070228" class="tel-num">
+                    +86-13396070228
+                  </a>
+                </p>
+              </div>
+              <div class="text-list-1">
+                <p class="text">
+                  Canada:
+                  <a href="tel:+1 6479243724" class="tel-num">
+                    +1-6479243724
+                  </a>
+                </p>
+              </div>
+              <div class="text-list-1">
+                <p class="text">
+                  Fax:
+                  <a href="tel:+86 27-87669028" className="font-bold">
+                    86-27-87669028
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
