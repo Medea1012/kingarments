93f73692e872f9f508b7585c398295105da67354
Medea Wang <medea.wangxy@gmail.com>
2023-12-30 13:20:28 -0500

    add items

--- client/src/pages/Apparels.jsx
+++ client/src/pages/Apparels.jsx
@@ -2,108 +2,109 @@ import React from "react";
 
 export default function Apparels() {
   return (
-    <section class="common-main">
-      <div class="contact-title">
-        <h2 class="title">Apparels</h2>
-      </div>
-      <div class="a-left-wrap">
-        <div class="page-container">
-          <div class="product-catelist">
-            <div class="product-cateitem">
-              <a
-                href="https://hagor.co.il/shop/soldiers/%d7%90%d7%91%d7%99%d7%96%d7%a8%d7%99%d7%9d/%d7%9e%d7%90%d7%a8%d7%96-2-%d7%97%d7%95%d7%9c%d7%a6%d7%95%d7%aa-t-%d7%9c%d7%90%d7%99%d7%a9%d7%94/"
-                class="cate-img lazy-wrap lazy-loaded"
-              >
-                <img
-                  class="lazy entered loaded"
-                  alt="Tshirts"
-                  data-ll-status="loaded"
-                  src="https://hagor.co.il/wp-content/uploads/2021/11/Tshirts_0006_7290116121789_2.jpg"
-                />
-                <span class="lazy-load"></span>
-              </a>
-              <a href="https://www.munibags.com/outdoor-bag/" class="cate-info">
-                <div class="cate-title">Outdoor T-shirt</div>
-                <div class="cate-text line2">
-                    T-shirt
-                </div>
-                <div class="cate-more">
-                  <em class="iconfont icon-jiahao"></em>
-                </div>
-              </a>
-            </div>
-            <div class="product-cateitem">
-              <a
-                href="https://hagor.co.il/shop/winter-clothing/%d7%97%d7%a8%d7%9e%d7%95%d7%a0%d7%99%d7%95%d7%aa/%d7%97%d7%a8%d7%9e%d7%95%d7%a0%d7%99%d7%aa-%d7%a4%d7%99%d7%a7/"
-                class="cate-img lazy-wrap lazy-loaded"
-              >
-                <img
-                  class="lazy entered loaded"
-                  alt="Cycling"
-                  data-ll-status="loaded"
-                  src="https://hagor.co.il/wp-content/uploads/2020/10/pik_s_flip.jpg"
-                />
-                <span class="lazy-load"></span>
-              </a>
-              <a href="https://www.munibags.com/cycling/" class="cate-info">
-                <div class="cate-title">cloth</div>
-                <div class="cate-text line2">
-                  This cloth is specifically designed for outdoor sport use.
-                </div>
-                <div class="cate-more">
-                  <em class="iconfont icon-jiahao"></em>
-                </div>
-              </a>
-            </div>
-            <div class="product-cateitem">
-              <a
-                href="https://www.munibags.com/dry-bag/"
-                class="cate-img lazy-wrap lazy-loaded"
-              >
-                <img
-                  class="lazy entered loaded"
-                  alt="Dry Bag"
-                  data-ll-status="loaded"
-                  src="https://hagor.co.il/wp-content/uploads/2020/10/Termi_0012_7290106321960_3.jpg"
-                />
-                <span class="lazy-load"></span>
-              </a>
-              <a href="https://www.munibags.com/dry-bag/" class="cate-info">
-                <div class="cate-title">Jeans</div>
-                <div class="cate-text line2">
-                  Made of tough yet lightweight nylon ripstop-TPU coated
-                  material.
-                </div>
-                <div class="cate-more">
-                  <em class="iconfont icon-jiahao"></em>
-                </div>
-              </a>
-            </div>
-            <div class="product-cateitem">
-              <a
-                href="https://www.munibags.com/military-bag/"
-                class="cate-img lazy-wrap lazy-loaded"
-              >
-                <img
-                  class="lazy entered loaded"
-                  alt="Military Bag"
-                  data-ll-status="loaded"
-                  src="https://hagor.co.il/wp-content/uploads/2020/10/softShell_0003_7290116122892_3.jpg"
-                />
-                <span class="lazy-load"></span>
-              </a>
-              <a
-                href="https://www.munibags.com/military-bag/"
-                class="cate-info"
-              >
-                <div class="cate-title">jackets</div>
-                <div class="cate-text line2">
-                    jackets
-                </div>
-                <div class="cate-more">
-                  <em class="iconfont icon-jiahao"></em>
-                </div>
-              </a>
+    <section>
+      <div class="common-main">
+        <div class="contact-title">
+          <h2 class="title">Apparels</h2>
+        </div>
+        <div class="a-left-wrap">
+          <div class="page-container">
+            <div class="product-catelist">
+              <div class="product-cateitem">
+                <a
+                  href="https://hagor.co.il/shop/soldiers/%d7%90%d7%91%d7%99%d7%96%d7%a8%d7%99%d7%9d/%d7%9e%d7%90%d7%a8%d7%96-2-%d7%97%d7%95%d7%9c%d7%a6%d7%95%d7%aa-t-%d7%9c%d7%90%d7%99%d7%a9%d7%94/"
+                  class="cate-img lazy-wrap lazy-loaded"
+                >
+                  <img
+                    class="lazy entered loaded"
+                    alt="Tshirts"
+                    data-ll-status="loaded"
+                    src="https://hagor.co.il/wp-content/uploads/2021/11/Tshirts_0006_7290116121789_2.jpg"
+                  />
+                  <span class="lazy-load"></span>
+                </a>
+                <a
+                  href="https://www.munibags.com/outdoor-bag/"
+                  class="cate-info"
+                >
+                  <div class="cate-title">Outdoor T-shirt</div>
+                  <div class="cate-text line2">T-shirt</div>
+                  <div class="cate-more">
+                    <em class="iconfont icon-jiahao"></em>
+                  </div>
+                </a>
+              </div>
+              <div class="product-cateitem">
+                <a
+                  href="https://hagor.co.il/shop/winter-clothing/%d7%97%d7%a8%d7%9e%d7%95%d7%a0%d7%99%d7%95%d7%aa/%d7%97%d7%a8%d7%9e%d7%95%d7%a0%d7%99%d7%aa-%d7%a4%d7%99%d7%a7/"
+                  class="cate-img lazy-wrap lazy-loaded"
+                >
+                  <img
+                    class="lazy entered loaded"
+                    alt="Cycling"
+                    data-ll-status="loaded"
+                    src="https://hagor.co.il/wp-content/uploads/2020/10/pik_s_flip.jpg"
+                  />
+                  <span class="lazy-load"></span>
+                </a>
+                <a href="https://www.munibags.com/cycling/" class="cate-info">
+                  <div class="cate-title">cloth</div>
+                  <div class="cate-text line2">
+                    This cloth is specifically designed for outdoor sport use.
+                  </div>
+                  <div class="cate-more">
+                    <em class="iconfont icon-jiahao"></em>
+                  </div>
+                </a>
+              </div>
+              <div class="product-cateitem">
+                <a
+                  href="https://www.munibags.com/dry-bag/"
+                  class="cate-img lazy-wrap lazy-loaded"
+                >
+                  <img
+                    class="lazy entered loaded"
+                    alt="Dry Bag"
+                    data-ll-status="loaded"
+                    src="https://hagor.co.il/wp-content/uploads/2020/10/Termi_0012_7290106321960_3.jpg"
+                  />
+                  <span class="lazy-load"></span>
+                </a>
+                <a href="https://www.munibags.com/dry-bag/" class="cate-info">
+                  <div class="cate-title">Jeans</div>
+                  <div class="cate-text line2">
+                    Made of tough yet lightweight nylon ripstop-TPU coated
+                    material.
+                  </div>
+                  <div class="cate-more">
+                    <em class="iconfont icon-jiahao"></em>
+                  </div>
+                </a>
+              </div>
+              <div class="product-cateitem">
+                <a
+                  href="https://www.munibags.com/military-bag/"
+                  class="cate-img lazy-wrap lazy-loaded"
+                >
+                  <img
+                    class="lazy entered loaded"
+                    alt="Military Bag"
+                    data-ll-status="loaded"
+                    src="https://hagor.co.il/wp-content/uploads/2020/10/softShell_0003_7290116122892_3.jpg"
+                  />
+                  <span class="lazy-load"></span>
+                </a>
+                <a
+                  href="https://www.munibags.com/military-bag/"
+                  class="cate-info"
+                >
+                  <div class="cate-title">jackets</div>
+                  <div class="cate-text line2">jackets</div>
+                  <div class="cate-more">
+                    <em class="iconfont icon-jiahao"></em>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
